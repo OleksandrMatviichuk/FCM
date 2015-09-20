5974bb6a6f9633e46c21ee8d2b96963819e37332
Popricoly <sasha.ansis@mail.ru>
2015-09-20 03:43:28 +0300

    add class for read data in file

---
+++ src/file.cpp
@@ -0,0 +1,53 @@
+#include<fstream>
+
+#include"file.h"
+
+file_class::file_class():num(0),
+			 len(0)
+{
+	data=NULL;
+}
+
+file_class::~file_class()
+{
+	if(num!=0)//if data matrix created, delete her
+	{
+		for(int i=0;i<num;i++)
+			delete [] data[i];
+		delete [] data;
+	}
+}
+bool file_class::read_data(string& name/* read flags*/)
+{
+	ifstream file(name.data());
+	if(!file.is_open())// file exist&
+	{
+		//read data block
+		//end read date block
+		file.close();
+		return true;
+	}	
+
+	return false;
+
+}
+
+double** file_class::get_data(int &n, int&l)
+{
+//return sizes data
+	n=num;
+	l=len;
+// if data not NULL copy data to array tmp and return tmp or return NULL
+	if(num!=0)
+	{
+		double** tmp=new double*[num];
+		for(int i=0;i<num;i++)
+			tmp[i]=new double[len];
+		for(int i=0;i<num;i++)
+			for(int j=0;j<num;j++)
+				tmp[i][j]=data[i][j];
+		return tmp;
+	}
+	return NULL;
+	
+}
---
+++ src/file.h
@@ -0,0 +1,22 @@
+#ifndef FILE_CLASS
+#define FILE_CLASS
+
+#include<fstream>
+#include<string>
+using namespace std;
+class file_class
+{
+public:
+	file_class();
+	~file_class();
+	bool read_data(string & name);	//inpun name file with data and read they
+	double** get_data(int&n, int&l);//return new data matrix
+	
+private:
+	double** data;		// matrix width data in file
+	int num;		//number vectors
+	int len;		//lenght vectors
+	bool data_readed;	//matrix=NULL?
+};
+
+#endif
