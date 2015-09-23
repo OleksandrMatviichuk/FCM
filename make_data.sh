6b186fb6d5cc5fe2d1b8ff381d31947d4bae1461
hamsternik <hamsternik9@gmail.com>
2015-09-23 07:13:40 +0300

    Remove needless script finding matrix dimension

--- make_data.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-heihgt=`wc -l $1 |  awk '{print $1}'`
-all_numb=`wc -w $1 | awk '{print $1}'`
-let "width = all_numb  / heihgt"
-answ="${heihgt} ${width}"
-
-eval "sed -i '1s/^/${answ}\n/' $1"
